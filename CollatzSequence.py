41f8963e740033f44d14b2050b122f583a23bdeb
Lohith-adiver <lohithk.24.beai@acharya.ac.in>
2025-05-16 11:27:18 +0530

    Create CollatzSequence.py

---
+++ CollatzSequence.py
@@ -0,0 +1,18 @@
+def collatz(n):
+    while n != 1:
+        print(n, end=' → ')
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+    print(1)
+
+try:
+    num = int(input("Enter a positive integer: "))
+    if num > 0:
+        print("Collatz sequence:")
+        collatz(num)
+    else:
+        print("Please enter a positive number.")
+except ValueError:
+    print("Invalid input. Please enter an integer.")
