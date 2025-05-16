94326a405adedbb2ee7d32a11714cd6fd9c5d750
Lohith-adiver <lohithk.24.beai@acharya.ac.in>
2025-05-16 11:31:50 +0530

    Create  CoinFlipStreaks.py

---
+++ CoinFlipStreaks.py
@@ -0,0 +1,18 @@
+import random
+
+number_of_experiments = 10000
+streak_length = 6
+streaks_found = 0
+
+for _ in range(number_of_experiments):
+    flips = []
+    for i in range(100):  
+        flips.append(random.choice(['H', 'T']))
+
+
+    for i in range(len(flips) - streak_length + 1):
+        if all(f == flips[i] for f in flips[i:i + streak_length]):
+            streaks_found += 1
+            break  
+print(f"Streaks of {streak_length} found in {streaks_found} out of {number_of_experiments} experiments.")
+print(f"Chance of streak: {streaks_found / number_of_experiments * 100:.2f}%")
