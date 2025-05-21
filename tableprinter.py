27758eddb1091e199ec790ac7ebd5d4be940725e
Lohith-adiver <lohithk.24.beai@acharya.ac.in>
2025-05-21 06:53:51 +0530

    Create tableprinter.py

---
+++ tableprinter.py
@@ -0,0 +1,11 @@
+
+print("Name   : Lohith Adiver")
+print("USN    : 1AY24AI063")
+print("Section: O")
+
+num = int(input("Enter a number to print its multiplication table: "))
+limit = int(input("e till which number to multiply: "))
+
+print("Multiplication Table for", num, "up to", limit)
+for i in range(1, limit + 1):
+    print(num, "x", i, "=", num * i)
