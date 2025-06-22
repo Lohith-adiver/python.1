af4b32b0750811f38c5375f8b5c13a428db2d228
Lohith-adiver <lohithk.24.beai@acharya.ac.in>
2025-06-22 22:39:23 +0530

    Create debug coin toss.py

---
+++ debug coin toss.py
@@ -0,0 +1,22 @@
+print("Name   : Lohith Adiver")
+print("USN    : 1AY24AI063")
+print("Section: O")
+
+import random
+
+guess = ''
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input()
+
+toss = 'heads' if random.randint(0, 1) == 1 else 'tails'
+
+if toss == guess:
+    print('You got it!')
+else:
+    print('Nope! Guess again!')
+    guess = input()
+    if toss == guess:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
