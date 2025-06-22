5ee5cb37d915cb8cbd3027f1aade56b58cda9820
Lohith-adiver <lohithk.24.beai@acharya.ac.in>
2025-06-22 23:01:36 +0530

    Create kang+roo .py

---
+++ kang+roo .py
@@ -0,0 +1,22 @@
+print("Name   : Lohith Adiver")
+print("USN    : 1AY24AI063")
+print("Section: O")
+
+class Kangaroo:
+    def __init__(self):
+        self.pouch_contents = []
+
+    def put_in_pouch(self, item):
+        self.pouch_contents.append(item)
+
+    def __str__(self):
+        return f'Kangaroo with pouch contents: {self.pouch_contents}'
+
+kanga = Kangaroo()
+roo = Kangaroo()
+
+kanga.put_in_pouch('apple')
+kanga.put_in_pouch(roo)
+
+print(kanga)
+print(roo)
